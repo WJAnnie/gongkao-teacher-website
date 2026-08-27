cb0814acd695f4291ca249e83393ed20d48a5d30
WJAnnie <50095919+WJAnnie@users.noreply.github.com>
2026-08-27 16:58:27 +0800

    加载表达规则阅读优化样式

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ import './shenlun/framework/framework-expression-stepper.css';
 import './shenlun/framework/framework-manual.css';
 import './shenlun/framework/framework-expression-article.css';
 import './shenlun/framework/framework-expression-polish.css';
+import './shenlun/framework/framework-expression-reading-refine.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.SITE_URL ?? 'http://localhost:3000'),
